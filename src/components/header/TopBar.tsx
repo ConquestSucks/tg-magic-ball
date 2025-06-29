f8124ef97d81b0d2bc611c83845e4252cb48f972
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-06-29 22:51:43 +0500

    Update TopBar.tsx

--- src/components/header/TopBar.tsx
+++ src/components/header/TopBar.tsx
@@ -2,18 +2,18 @@ import { useLaunchParams } from '@telegram-apps/sdk-react'
 import { User as UserIcon } from 'lucide-react'
 
 export const TopBar = () => {
-  const params = useLaunchParams()
-  const user = params?.tgWebAppData?.user
+    const params = useLaunchParams()
+    const user = params?.tgWebAppData?.user
 
-  return (
-    <div className='w-full flex items-center justify-between px-4 py-2'>
-      <h1 className='text-white font-bold'>ASK THE ORB</h1>
-      {user?.photo_url ? (
-        <img src={user.photo_url} className='w-8 h-8 rounded-full' />
-      ) : (
-        <UserIcon className='w-6 h-6 text-white' />
-      )}
-      <div className='w-8'></div>
-    </div>
-  )
+    return (
+        <div className='w-full flex items-center justify-between px-4 py-2'>
+            <div className='w-8'></div>
+            <h1 className='text-white font-bold'>ASK THE ORB</h1>
+            {user?.photo_url ? (
+                <img src={user.photo_url} className='w-8 h-8 rounded-full' />
+            ) : (
+                <UserIcon className='w-6 h-6 text-white' />
+            )}
+        </div>
+    )
 } 
