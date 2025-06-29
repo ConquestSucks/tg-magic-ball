31727b7f1576381d42f7a837af98bcc9eec88bfb
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-06-29 22:05:22 +0500

    Revert "tg miniapp height adaptation"
    This reverts commit 73abdad9fe0d3c3e1295b5609bf9de27c1478a57.

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,10 @@ import './App.css'
 import MagicBall from './components/magic-ball/MagicBall'
 import NavMenu from './components/navbar/NavMenu'
 import { observer } from "mobx-react"
-import { useEffect } from 'react'
 
 const App = observer(() => {
-    useEffect(() => {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const tg = (window as any).Telegram?.WebApp
-        if (!tg) return
-
-        const applyHeight = () => {
-            const h = tg.viewportHeight ?? window.innerHeight
-            document.documentElement.style.setProperty('--tg-vh', `${h}px`)
-        }
-
-        applyHeight()
-        tg.onEvent('viewportChanged', applyHeight)
-        return () => tg.offEvent?.('viewportChanged', applyHeight)
-    }, [])
-
     return (
-        <div className='flex flex-col items-center justify-between bg-[#3A3939] select-none overflow-auto' style={{ height: 'var(--tg-vh)' }}>
+        <div className='flex flex-col h-screen items-center justify-between bg-[#3A3939]'>
             <h1>ASK THE ORB</h1>
             <MagicBall width={220} height={220} />
             <NavMenu />
