8cbba05deafff344e26bca1fabbddafe230c38cf
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-06-29 21:45:27 +0500

    Revert "mobile keyboard adaptation"
    This reverts commit 546114a33e23c18ac682ce8449b36a17bef08ffa.

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,12 @@ import './App.css'
 import MagicBall from './components/magic-ball/MagicBall'
 import NavMenu from './components/navbar/NavMenu'
 import { observer } from "mobx-react"
-import { useState, useEffect } from 'react'
 
 const App = observer(() => {
-    const [vhOffset, setVhOffset] = useState(0)
-
-    useEffect(() => {
-        const handler = () => {
-            const visual = window.visualViewport
-            if (!visual) return
-            setVhOffset(window.innerHeight - visual.height)  // высота клавиатуры
-        }
-        window.visualViewport?.addEventListener('resize', handler)
-        return () => window.visualViewport?.removeEventListener('resize', handler)
-    }, [])
-
     return (
-        <div className='flex flex-col h-screen items-center justify-between bg-[#3A3939] select-none' style={{ paddingBottom: vhOffset }}>
+        <div className='flex flex-col h-screen items-center justify-between bg-[#3A3939]'>
             <h1>ASK THE ORB</h1>
-            <div className="
-                relative inline-block
-                w-[min(90vw,280px)]
-                max-h-[calc(60vh-120px)]
-                overflow-y-auto
-                px-5 py-4 rounded-3xl shadow-lg bg-white text-black text-sm
-            ">
-                <MagicBall width={220} height={220} />
-            </div>
+            <MagicBall width={220} height={220} />
             <NavMenu />
         </div>
     )
